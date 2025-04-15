8452722d55a9bf8cdd8a0f8197efbbd1a00a1e82
prasannaweb-ui <tcpprasanna@gmail.com>
2025-04-15 09:21:43 +0530

    homepage resolved

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/tcp-international/',
   plugins: [react()],
+  base: '/tcp-international/',
 
 });
